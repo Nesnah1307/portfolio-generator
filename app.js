6b3062c97f74d8e2f1e7b9ba55f5fc348e702be9
MackH <96394025+Nesnah1307@users.noreply.github.com>
2022-03-18 17:02:35 -0600

    appending HTML elements

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
 const inquirer = require('inquirer');
 
-// const fs = require('fs');
+const fs = require('fs');
 
-// const generatePage = require('./src/page-template.js');
-
-// const pageHTML = generatePage(name, github);
+const generatePage = require('./src/page-template.js');
 
 
-// fs.writeFile('./index.html', pageHTML, err => {
-//     if (err) throw err;
-
-//     console.log('Portfolio complete! Check out index.html to see the output!');
-//   });
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -146,8 +139,12 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('./index.html', pageHTML, err => {
+            if (err) throw err;
     });
+});
 
 
 
